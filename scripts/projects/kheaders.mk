a092acf7fbd76af8421a9b66b7d81e3febdcdc7e
BigfootACA <bigfoot@classfun.cn>
2021-03-30 22:52:02 +0800

    remove folder build/progress

--- scripts/projects/kheaders.mk
+++ scripts/projects/kheaders.mk
@@ -1,16 +1,16 @@
-build/progress/kheaders: build/linux/Makefile
+build/sysroot/usr/include/.kheaders: build/linux/Makefile
 	@$(MAKE) \
 		-C build/linux \
 		INSTALL_HDR_PATH="$(PWD)/build/sysroot/usr" \
 		$(KHEADERS_INSTALL_FLAGS) \
 		headers_install
-	@echo > $@
-build/progress/headers: build/headers
+	@touch $@
+build/sysroot/usr/include/.headers: build/headers
 	@mkdir -pv "$(PWD)/build/sysroot/usr/include/"
 	@cp -av $</* "$(PWD)/build/sysroot/usr/include/"
-	@echo > $@
-install-headers: build/progress/headers
-install-kheaders: build/progress/kheaders
+	@touch $@
+install-headers: build/sysroot/usr/include/.headers
+install-kheaders: build/sysroot/usr/include/.kheaders
 clean-kheaders: build/linux/.git
 	@cd $(shell dirname $<)&&git clean -xdf&&git reset --hard
 .PHONY: clean-kheaders install-kheaders install-headers
--- scripts/projects/musl.mk
+++ scripts/projects/musl.mk
@@ -6,7 +6,7 @@ build/musl/config.mak: build/musl/configure
 build/sysroot/usr/lib/musl.specs: build/musl.specs.in
 	@mkdir -vp build/sysroot/usr/lib
 	@sed "s|%SYSROOT%|$(PWD)/build/sysroot|g;s|%GCC%|$(CROSS_COMPILE)gcc|g" < $< > $@
-build/musl-gcc: build/musl-gcc.in build/sysroot/usr/lib/musl.specs build/sysroot/usr/lib/libc.so build/progress/kheaders build/progress/headers
+build/musl-gcc: build/musl-gcc.in build/sysroot/usr/lib/musl.specs build/sysroot/usr/lib/libc.so build/sysroot/usr/include/.kheaders build/sysroot/usr/include/.headers
 	@sed "s|%SYSROOT%|$(PWD)/build/sysroot|g;s|%GCC%|$(CROSS_COMPILE)gcc|g" < $< > $@
 	@chmod +x $@
 build/musl/lib/libc.so: build/linux/Makefile build/musl/config.mak
