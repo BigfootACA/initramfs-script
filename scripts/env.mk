f5956e628b9ba49e1d641876b28a879a1276dd5d
BigfootACA <bigfoot@classfun.cn>
2021-04-04 18:01:24 +0800

    env.mk: TARGET required

--- scripts/env.mk
+++ scripts/env.mk
@@ -5,11 +5,12 @@ INSTALL?=install
 BIN_INSTALL=$(INSTALL) --strip-program="$(XSTRIP)" -svDm755
 SYSROOT=$(PWD)/build/sysroot
 REALCC?=$(PWD)/build/musl-gcc
-ifdef TARGET
+ifndef TARGET
+$(error TARGET not specified)
+endif
 ifndef CROSS_COMPILE
 CROSS_COMPILE=$(TARGET)-
 export CROSS_COMPILE
-endif
 endif
 export REALCC
 export SYSROOT
